47c035d09fa01c0988e819f248e6f6e24e06ea8f
Rohan <97392604+Rohanchaursiya@users.noreply.github.com>
2025-08-27 18:15:32 +0530

    "Update Time Bomb"

--- Async Programming & Promises/P1_TimeBomb.js
+++ Async Programming & Promises/P1_TimeBomb.js
@@ -1,25 +1,16 @@
-// function CountDown(num) {
-//     for (let counter = num; counter >= 1; counter--) {
-//         setTimeout(function () {
-//             if (counter == 1) {
-//                 console.log(counter);
-//                 console.log("Count Down completed! Boom..!");
-//             } else {
-//                 console.log(counter);
-//             }
+function CountDown(num) {
+    for (let counter = num; counter >= 1; counter--) {
+        setTimeout(function () {
+            if (counter == 1) {
+                console.log(counter);
+                console.log("Count Down completed! Boom..!");
+            } else {
+                console.log(counter);
+            }
+        }, (num + 1 - counter) * 1000);
+    }
+}
 
-//         }, (num + 1 - counter) * 1000);
-//     }
-// }
-
-// let num = 10;
-// CountDown(num);
-
-const arr = [1, 2, , 4];
-console.log(arr.length);
-
-arr.map(num => console.log("map:", num));
-
-
-
+let num = 10;
+CountDown(num);
 
